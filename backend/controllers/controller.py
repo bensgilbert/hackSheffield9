4055cad31ec606640c695ba2adaf900a3ed7f338
Jonathan Lupu <jonathan.lupu@gmail.com>
2024-11-16 16:59:05 +0000

    Extended controller.py

--- backend/controllers/controller.py
+++ backend/controllers/controller.py
@@ -1,12 +1,20 @@
 # Basic controller set up - will send information between database and front end
-
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 # Creates flask object
 app = Flask(__name__)
 
+# Route following /
+# Returns needed information for the given page
 @app.route("/")
 def index():
     return "Hello"
 
+@app.route("/login")
+def login():
+    return "login"
 
+@app.route("/users/<int:user_id>")
+def users_api(user_id):
+    # user_id is passed directly as a parameter
+    return jsonify({"message": f"User ID is {user_id}"})
