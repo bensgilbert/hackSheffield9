5b3253c31bb375316903b442967ac0239fa4f826
Ben Gilbert <bsamuel-gilbert1@sheffield.ac.uk>
2024-11-16 14:39:03 +0000

    Add demo page including map

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	let mapElement;
+	let map;
+
+	onMount(() => {
+		loadGoogleMaps();
+	});
+
+	async function loadGoogleMaps() {
+		const pkg = await import('@googlemaps/js-api-loader');
+		const { Loader } = pkg;
+
+		const loader = new Loader({
+			apiKey: "cheeky-bastard",
+			version: 'weekly',
+			libraries: ['places', 'maps']
+		});
+
+		try {
+			await loader.load();
+			initMap();
+		} catch (e) {
+			console.error('Error loading Google Maps', e);
+		}
+	}
+
+	function initMap() {
+		const google = window.google;
+		map = new google.maps.Map(mapElement, {
+			center: { lat: 13.736717, lng: 100.523186 },
+			zoom: 8,
+			disableDefaultUI: true,
+			gestureHandling: 'greedy',
+			mapId: 'your-map-id' // Replace or remove this
+		});
+
+		const marker = new google.maps.Marker({
+			position: { lat: 13.7434332, lng: 100.5534927 },
+			map: map,
+			title: 'Hello World!'
+		});
+
+		marker.addListener('click', function () {
+			alert('Marker was clicked!');
+		});
+	}
+</script>
+
+<div bind:this={mapElement} class="size-96" id="map"></div>
