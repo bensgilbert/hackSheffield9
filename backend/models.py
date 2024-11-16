4fb422aa08089ba093bd0850ee72def4614d9fa5
Ben Gilbert <bsamuel-gilbert1@sheffield.ac.uk>
2024-11-16 20:19:01 +0000

    DATABASE YAYYY

---
+++ backend/models.py
@@ -0,0 +1,57 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+from sqlalchemy import create_engine, select
+
+engine = create_engine("sqlite:///neighboury.db", echo=True)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Account(Base):
+    __tablename__ = "account"
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+
+class Order(Base):
+    __tablename__ = "order"
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    message: Mapped[str] = mapped_column(nullable=False)
+    account_id: Mapped[int] = mapped_column(nullable=False)
+    lat: Mapped[str] = mapped_column(nullable=False)
+    lng: Mapped[str] = mapped_column(nullable=False)
+    fufullied: Mapped[int] = mapped_column(nullable=True)
+
+
+class OrderItem(Base):
+    __tablename__ = "order_item"
+
+    account_id: Mapped[int] = mapped_column(primary_key=True)
+    order_id: Mapped[int] = mapped_column(primary_key=True)
+    item_id: Mapped[int] = mapped_column(nullable=False)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+
+
+class Item(Base):
+    __tablename__ = "item"
+
+    item_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+
+
+Base.metadata.create_all(engine)
+
+## Order
+# Fufilled
+# List of items
+# Message
+# Username
+# lat/lng
+# timeframe (start/end)
+
+## Item
+# Name
+# Quantity
