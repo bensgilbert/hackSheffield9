1f37b19dc1659114580cdf35f315a32b7d3b419f
jedsmith2004 <90654430+jedsmith2004@users.noreply.github.com>
2024-11-17 06:39:43 +0000

    Fixed routing issue

---
+++ backend/static/_app/immutable/entry/start.y0A0B_YL.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BHK9mrpw.js";export{t as start};
--- frontend/src/routes/makerequest/+page.js
+++ frontend/src/routes/makerequest/+page.js
@@ -1,11 +1,11 @@
 import { goto } from '$app/navigation';
 
-export function load() {
-	// Request database
-	// fetch
-	// const hasRequest = false;
-	// if (hasRequest) {
-	//     goto("/viewrequest")
-	// }
-	goto('/viewrequest');
-}
+// export function load() {
+// 	// Request database
+// 	// fetch
+// 	// const hasRequest = false;
+// 	// if (hasRequest) {
+// 	//     goto("/viewrequest")
+// 	// }
+// 	goto('/viewrequest');
+// }
