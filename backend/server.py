4fc141bc6aea255dc7b253c28b0477050f43b8c7
acc23nn <159147963+acc23nn@users.noreply.github.com>
2024-11-17 04:14:41 +0000

    aekyjgfukzywef

--- backend/server.py
+++ backend/server.py
@@ -37,15 +37,16 @@ oauth.register(
 
 @app.route("/login")
 def login():
-    return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("callback", _external=True)
-    )
+    # return oauth.auth0.authorize_redirect(
+    #     redirect_uri=url_for("callback", _external=True)
+    # )
+    return oauth.auth0.authorize_redirect(redirect_uri='http://localhost:5173/callback')
 
 @app.route("/callback", methods=["GET", "POST"])
 def callback():
     token = oauth.auth0.authorize_access_token()
     session["user"] = token
-    return redirect("/")
+    return redirect('http://localhost:5173/')
 
 @app.route("/logout")
 def logout():
